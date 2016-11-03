f0b8c39c4801c4056b9f6c4f200fc04f72f763cb
JJ-xyz <JJ_xyz@iCloud.com>
2016-11-03 16:52:44 -0400

    More Changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  # before_action :authenticate, except: [:login, :create]
+  before_action :authenticate, except: [:login, :create]
 
   def login
     user = User.find_by(username: params[:user][:username])
@@ -11,28 +11,46 @@ class UsersController < ApplicationController
     end
   end
 
+  def create
+     user = User.new(user_params)
+     if user.save
+       render json: {status: 200, message: "ok"}
+     else
+       render json: {status: 422, user: user.errors}
+     end
+   end
+
+   def show
+     user = User.find(params[:id])
+
+     render json: {status: 200, user: user}
+   end
 
 
 
 
 
-private
+  private
 
-  def token(id, username)
-    JWT.encode(payload(id, username), ENV['JWT_SECRET'], 'HS256')
-  end
+    def token(id, username)
+      JWT.encode(payload(id, username), ENV['JWT_SECRET'], 'HS256')
+    end
 
-  def payload(id, username)
-    {
-      exp: (Time.now + 1.day).to_i # Expiration date 24 hours from now
-      iat: Time.now.to_i,
-      iss: ENV['JWT_ISSUER'],
-      user: {
-        id: id,
-        username: username
+    def payload(id, username)
+      {
+        exp: (Time.now + 1.day).to_i, # Expiration date 24 hours from now
+        iat: Time.now.to_i,
+        iss: ENV['JWT_ISSUER'],
+        user: {
+          id: id,
+          username: username
+        }
       }
-    }
   end
 
+
+  def user_params
+     params.required(:user).permit(:username, :password, :household_id)
+  end
 
 end
