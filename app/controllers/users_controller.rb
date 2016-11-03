e8147e6e22839ca46314a9535f0fc38630002063
JJ-xyz <JJ_xyz@iCloud.com>
2016-11-03 16:07:55 -0400

    Setup authentication Step 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,35 @@ class UsersController < ApplicationController
   def login
     user = User.find_by(username: params[:user][:username])
     if user && user.authenticate(params[:user][:password])
+      token = token(user.id, user.username)
       render json: {status: 201, user: user}
     else
       render json: {status: 401, message: "unauthorized"}
     end
   end
+
+
+
+
+
+
+private
+
+  def token(id, username)
+    JWT.encode(payload(id, username), ENV['JWT_SECRET'], 'HS256')
+  end
+
+  def payload(id, username)
+    {
+      exp: (Time.now + 1.day).to_i # Expiration date 24 hours from now
+      iat: Time.now.to_i,
+      iss: ENV['JWT_ISSUER'],
+      user: {
+        id: id,
+        username: username
+      }
+    }
+  end
+
 
 end
